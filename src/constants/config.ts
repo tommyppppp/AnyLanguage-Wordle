bd7b78754bfef4f399dade3105ad0442da4d5828
tommyppppp <137008089+tommyppppp@users.noreply.github.com>
2023-06-18 23:27:14 -0500

    Update config.ts
    Change start date

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -12,7 +12,7 @@ export const CONFIG = {
   googleAnalytics: '', // You can use this if you use Google Analytics
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
-  startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
+  startDate: 'June 18, 2023 00:00:00', // what date and time to start your game from
   defaultLang: 'en', // the default interface language
   availableLangs: ['en', 'es', 'sw', 'zh'], // the options available to the user for translation languages
   escapeSpecialCharacters: true, // whether to escape all characters in the orthography.
