04835b2ded69851be44276a165eb33b3f619fcbf
tommyppppp <137008089+tommyppppp@users.noreply.github.com>
2023-06-18 19:42:52 -0500

    Update config.ts Added TommyP author

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,7 +2,7 @@ export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
   language: 'YourLanguageHere', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
+  author: 'TommyP', // Put your name here so people know who made this Wordle!
   authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
   wordListSource: 'YourDictionarySource', // Describe the source material for your words here
   wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
